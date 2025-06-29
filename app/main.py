fa33ff92519855ae4a4ca015be7e74d5a03c186c
Inder <kumar.inder.com>
2025-06-30 01:27:02 +0530

    update

--- app/main.py
+++ app/main.py
@@ -3,7 +3,17 @@ from app.api.v1.endpoints import chat, recipes
 from fastapi.middleware.cors import CORSMiddleware
 
 
-app = FastAPI(title="DishCart AI Backend")
+app = FastAPI(title="DishCart AI Backend"
+    ,docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json"
+)
+
+origins = [
+    "https://dish-kart.netlify.app/",
+    "http://localhost:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
